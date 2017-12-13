8db10ac3fb891b4a7ec1847e4b6f42489eea3c93
dmfriendjr <davidmfriendjr@gmail.com>
2017-12-13 16:12:38 -0500

    Added nested components for star ratings

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //Our directives are declared here
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //External modules are declared here
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
